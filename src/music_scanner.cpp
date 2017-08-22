c685176095f184958145987ecdb87ebbd36c90e1
Harry He <hjhe@sfu.ca>
2017-08-21 17:53:29 -0700

    -Adding use of boost::interprocess for direct to file transfer of list of music files -changed printout to read from tofile results instead -Added many TODOs that will need to be completed later

--- src/music_scanner.cpp
+++ src/music_scanner.cpp
@@ -7,22 +7,47 @@
 
 #include "music_scanner.hpp"
 #include "music_type.hpp"
+#include <boost/interprocess/ipc/message_queue.hpp>
+using namespace boost::interprocess;
 namespace fs = std::experimental::filesystem;
 music_scanner::music_scanner()
 {
-	//TODO: Set default outfile
+	mq_name="music_scanner_mq";
+	/*
+	 * TODO: use timed send to see if anything is accessing queue, if something is
+	 * accessing the message queue then open the text file and read from top
+	 * May need to use boost::serialization & Protocol Buffers for more complex data later...
+	 */
+	message_queue temp(create_only     //only create
+			,mq_name.c_str()          //name
+			,MUSIC_SCANNER_MQ_SIZE     //max message number
+			,sizeof(char)              //max message size
+	);
+	outfile = "/music_scanner_output.txt";
 	path = "/home";
 }
 music_scanner::~music_scanner()
 {
+	message_queue::remove(mq_name.c_str());
 	//TODO: Close file IO
 }
 void music_scanner::printout()
 {
+	//TODO setup a quick read of the printout file.
+}
+/*
+ * TODO:run https://github.com/apetrone/simplefilewatcher or QtFileWatcher
+ * when change is detected schedule a rescan after activity has died down...
+ */
+void music_scanner::tofile()
+{
 	music_type is_music;
 	for (auto & p : fs::recursive_directory_iterator(path))
+	{
 		if(is_music(p.path()))
 		{
-			std::cout << p << std::endl;
+			//TODO: write to file
+			//std::cout << p << std::endl;
 		}
+	}
 }
--- src/music_scanner.hpp
+++ src/music_scanner.hpp
@@ -7,14 +7,15 @@
 
 #ifndef MUSIC_SCANNER_HPP_
 #define MUSIC_SCANNER_HPP_
-
+#define MUSIC_SCANNER_MQ_SIZE 1000
 #include <string>
 #include <iostream>
 #include <experimental/filesystem>
-
+#include <boost/interprocess/ipc/message_queue.hpp>
 class music_scanner
 {
 	public:
+	std::string mq_name;
 	std::string path;
 	std::string outfile;
 	void printout();
